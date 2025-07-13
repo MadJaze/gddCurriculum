9c2156fdb694a1978cde6ec9871554a8242ca05b
Jasiel Padron <madjaze.b1@gmail.com>
2025-07-13 17:39:30 -0400

    Menu for mobile users3

--- gdd.js
+++ gdd.js
@@ -1,25 +1,39 @@
 
-//Criei uma variavel 
+//Atribuindo variaveis variavel 
 
 const nav_mobile = document.getElementById("button-mobile");
 const menu = document.getElementById("menu");
+const nav_buttons = document.getElementsByClassName("nav-buttons");
 
 //Coloquei nela numa função click 
 
 nav_mobile.addEventListener("click", function(){
 
-console.log("Abrir opciones navegador mobile");
-
 if (menu.style.display == ("none")) {
 
-menu.style.display = ("block");
+menu.style.display = ("block"); //Mostra o menu
+document.body.style.overflow = "hidden"; //desativa o scrolling
 
 } else {
 
-menu.style.display = ("none");
+menu.style.display = ("none"); //oculta o menu
+document.body.style.overflow = "auto"; //ativa o scrolling
 
 }
 
 })
 
+// Função para os botões do site em mobile 
+
+for (let i = 0; i < nav_buttons.length; i++) {
+
+nav_buttons[i].addEventListener("click", function(){
+
+menu.style.display = ("none"); //oculta o menu
+document.body.style.overflow = "auto"; //ativa o scrolling
+
+
+})
+
+}
 
